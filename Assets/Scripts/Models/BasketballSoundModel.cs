de23e28e16195e08f16306774f18c660d98eb0a4
William Kirk <kirkwilliamj@gmail.com>
2025-06-09 22:56:10 -0500

    Removed unnecessary and unused scripts and prefabs from the project

--- Assets/Scripts/Models/BasketballSoundModel.cs
+++
@@ -1,7 +0,0 @@
-using Normal.Realtime.Serialization;
-[RealtimeModel(createMetaModel: true)]
-public partial class BasketballSoundModel
-{
-    [RealtimeProperty(4, true, true)]
-    private int _bounceAudioTrigger;
-}
